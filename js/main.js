7ed1495e1576b41626fb10b28acff48e35fd8edf
salis <vampir.66.66@yandex.ru>
2022-09-18 23:49:51 +0500

    allIsComplite

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Notebook', price: 2000, img: 'pic/notebook.jpg'},
+    {id: 2, title: 'Mouse', price: 20, img: 'pic/mouse.jpg'},
+    {id: 3, title: 'Keyboard', price: 200, img: 'pic/keyboard.jpg'},
+    {id: 4, title: 'Gamepad', price: 50, img: 'pic/gamepad.jpg'},
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
+const renderProduct = (obj) => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
+                <h3 class="titleProduct">${obj.title}</h3>
+                <img class="imgProduct" src = ${obj.img}>
+                <div>
+                    <p class="priceProduct">${obj.price}$</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price));
+    const productsList = list.map(item => renderProduct(item));
     console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = productsList.join(`\n`);
 };
 
 renderPage(products);
